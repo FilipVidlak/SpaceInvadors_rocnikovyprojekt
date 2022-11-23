924b899b66198397160a5a5016ca1e618f80a7a6
FilipVidlak <72192554+FilipVidlak@users.noreply.github.com>
2022-11-23 07:21:02 +0100

    Add files via upload

---
+++ SpaceInvadors_rocnikovyprojekt/SpaceInvadors_rocnikovyprojekt/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SpaceInvadors_rocnikovyprojekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.restart_button = new System.Windows.Forms.Button();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // restart_button
+            // 
+            this.restart_button.Location = new System.Drawing.Point(302, 202);
+            this.restart_button.Name = "restart_button";
+            this.restart_button.Size = new System.Drawing.Size(144, 74);
+            this.restart_button.TabIndex = 0;
+            this.restart_button.Text = "Restart";
+            this.restart_button.UseVisualStyleBackColor = true;
+            this.restart_button.Click += new System.EventHandler(this.restart_button_Click);
+            // 
+            // exit_button
+            // 
+            this.exit_button.Location = new System.Drawing.Point(302, 318);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(144, 67);
+            this.exit_button.TabIndex = 1;
+            this.exit_button.Text = "Ukončit";
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(348, 474);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 67);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label4.Location = new System.Drawing.Point(13, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 30);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Úroveň:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(113, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 30);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "01";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(637, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 30);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Skóre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(730, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 30);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(211, 84);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(344, 60);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "KONEC HRY";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.restart_button);
+            this.Name = "Form1";
+            this.Text = "Space Invadors";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button restart_button;
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SpaceInvadors_rocnikovyprojekt/SpaceInvadors_rocnikovyprojekt/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SpaceInvadors_rocnikovyprojekt
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void restart_button_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*WindowsMediaPlayer soundtrack = new WindowsMediaPlayer();
+             WindowsMediaPlayer strela = new WindowsMediaPlayer();
+            WindowsMediaPlayer exploze = new WindowsMediaPlayer();
+            soundtrack.URL = "songs\\soundtrack.mp3"
+            strela.URL = "songs\\strela.mp3";
+            exploze.URL = "songs\\exploze.mp3";
+            soundtrack.settings.setMode("loop", true);
+             */
+            label5.Visible = false;
+            restart_button.Visible = false;
+            exit_button.Visible = false;
+            PictureBox[] neprlode = new PictureBox[10];
+            PictureBox[] neprmun = new PictureBox[10];
+            PictureBox[] municehrace = new PictureBox[4];
+            PictureBox[] hvezdy = new PictureBox[8];
+            Random rnd = new Random();
+            /*
+            Image enemi1 = Image.FromFile("asserts\\e1.png");
+            Image enemi2 = Image.FromFile("asserts\\e2.png");
+            Image enemi3 = Image.FromFile("asserts\\e3.png");
+            Image boss1 = Image.FromFile("asserts\\boss1.png");
+            Image boss2 = Image.FromFile("asserts\\boss2.png");
+            Image municehrace = Image.FromFile("asserts\\municehrace.png");
+            - grafiku krome sve vesmirne lode jsem zatim neresil
+            */
+
+            //inicializace nepratelskych lodi
+            for (int i = 0; i < neprlode.Length; i++)
+            {
+                neprlode[i] = new PictureBox();
+                neprlode[i].Size = new Size(35, 35);
+                //neprlode[i].SizeMode = PictureBoxSizeMode.Zoom;
+                neprlode[i].Visible = true;
+                neprlode[i].BackColor = Color.Black; // pro ukazku
+                //neprlode[i].BorderStyle = BorderStyle.None;
+                neprlode[i].Location = new Point((i + 1) * 50, 10); //jinak ve vysce ma byt zaporna hodnota, aby priletali shora, ale to vse az budu mit timer 
+                this.Controls.Add(neprlode[i]);
+            }
+            /*
+            neprlode[0].Image = boss1;
+            neprlode[1].Image = enemi2;
+            neprlode[2].Image = enemi3;
+            neprlode[3].Image = enemi3;
+            neprlode[4].Image = enemi1;
+            neprlode[5].Image = enemi3;
+            neprlode[6].Image = enemi2;
+            neprlode[7].Image = enemi3;
+            neprlode[8].Image = enemi2;
+            neprlode[9].Image = boss2;
+            */
+            //inicialize munice nepratelskych lodi
+            for (int i = 0; i < neprmun.Length; i++)
+            {
+                neprmun[i] = new PictureBox();
+                neprmun[i].Size = new Size(2, 20);
+                neprmun[i].BackColor = Color.Red;
+                neprmun[i].Visible = true;
+                int random = rnd.Next(0, 10);
+                neprmun[i].Location = new Point(neprlode[random].Location.X, neprlode[random].Location.Y + 40); // jen pro ukazku to mam zase kladnou hodnotu pri lokaci na vysku
+                this.Controls.Add(neprmun[i]);
+            }
+            //inicializace munice hrace
+            for (int i = 0; i < municehrace.Length; i++)
+            {
+                municehrace[i] = new PictureBox();
+                municehrace[i].Size = new Size(6, 6);
+                //munice[i].Image = municehrace;
+                municehrace[i].SizeMode = PictureBoxSizeMode.Zoom;
+                municehrace[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(municehrace[i]);
+            }
+            //inicializace hvezd na pozadi
+            for (int i = 0; i < hvezdy.Length; i++)
+            {
+                hvezdy[i] = new PictureBox();
+                hvezdy[i].BorderStyle = BorderStyle.None;
+                int sirka = rnd.Next(20, 580);
+                int vyska = rnd.Next(10, 400); // - zase jen pro ukazku, jinak zaporna hodnota, aby priletali seshora
+                hvezdy[i].Location = new Point(sirka, vyska);
+                if (i % 2 == 0)
+                {
+                    hvezdy[i].Size = new Size(3, 3);
+                    hvezdy[i].BackColor = Color.WhiteSmoke;
+                }
+                else
+                {
+                    hvezdy[i].Size = new Size(4, 4);
+                    hvezdy[i].BackColor = Color.DarkGray;
+                }
+                this.Controls.Add(hvezdy[i]);
+            }
+            /*soundtrack.controls.play();*/
+        }
+    }
+}
